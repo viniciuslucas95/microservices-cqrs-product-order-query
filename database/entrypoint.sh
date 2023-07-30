803fec81481908b41b88fabee253a71ce55decd8
Vinícius Lucas <viniciuslucas1564@gmail.com>
2023-07-30 11:09:11 -0300

    Finished replication setup

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-export PGPASSWORD='admin'
-
-psql -U 'admin' <<- EOSQL
-    CREATE DATABASE dev;
-EOSQL
-
-psql -U 'admin' -d 'dev' <<- EOSQL
-	CREATE TABLE "product_orders" (
-        id UUID PRIMARY KEY,
-        product_id UUID NOT NULL,
-        user_id UUID NOT NULL,
-        date TIMESTAMP WITH TIME ZONE NOT NULL,
-        status VARCHAR(9) NOT NULL
-    );
-EOSQL
+export PGPASSWORD='rep_admin'
 
 pg_ctl stop -D /var/lib/postgresql/data -m fast
 
+until psql -h postgres-product-order-command -U rep_admin -d dev -c "SELECT 1" >/dev/null 2>&1; do
+  echo "Waiting for the primary database to be ready..."
+  sleep 1
+done
+
+rm -rf /var/lib/postgresql/data 2>/dev/null
+
+pg_basebackup -D /var/lib/postgresql/data -h postgres-product-order-command -X stream -c fast -U rep_admin -W
+
 cp /docker-entrypoint-initdb.d/pg_hba.conf /var/lib/postgresql/data/
 cp /docker-entrypoint-initdb.d/postgresql.conf /var/lib/postgresql/data/
+cp /docker-entrypoint-initdb.d/standby.signal /var/lib/postgresql/data/
 
 /usr/local/bin/docker-entrypoint.sh postgres
